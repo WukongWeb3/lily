ad7027d8dd61f94a5e1f0ea0f9a469e2918b2897
WukongWeb3 <121208725+WukongWeb3@users.noreply.github.com>
2025-02-24 11:53:42 +0800

    init

---
+++ index.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# if "a" not in st.session_state:
+#     st.session_state.a = 0
+# clicked = st.button("加1")
+# if clicked:
+#     st.session_state.a += 1
+# st.write(st.session_state.a)
+
+st.title("林老师是个大美女")
+st.image("./lily.jfif", width=2400)
